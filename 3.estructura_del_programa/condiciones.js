cba884bae3936f7d9aa7f6d9cf1b6af5dd31cf25
yadirabot11 <medinaazuyadira@gmail.com>
2024-10-15 19:07:16 +0200

    feet: clase de condicion simple
    vimos ejemplos del usuario de una condicion simple con cuerpo y condicion simple de una sola linea

---
+++ 3.estructura_del_programa/condiciones.js
@@ -0,0 +1,12 @@
+//ejemplo de condicion simple 
+
+//capturamos la edad y lo convertimos a numero
+let numero = Number(prompt("ingresa tu edad"))
+//verificamos si el tipo de datos edad es number realmente
+if (typeof edad == "number"){
+    //si las comparacion es verdad mostraremos por consola la raiz cuadra de edad
+    console.log(edad*edad)
+};
+
+//ejemplo de una condicion simple de una sola linea 
+if(1+1==2) console.log("Es verdad");
